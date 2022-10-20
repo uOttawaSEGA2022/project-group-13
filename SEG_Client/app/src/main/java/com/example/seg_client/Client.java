56c27b17aaf6183ce163f2f8ee351d533cfa10ba
Maisha <mhabi068@uottawa.ca>
2022-10-20 12:38:41 -0400

    added class plus more

---
+++ SEG_Client/app/src/main/java/com/example/seg_client/Client.java
@@ -0,0 +1,23 @@
+package com.example.seg_client;
+
+public class Client extends User {
+
+    private String creditCardInfo;
+
+    public Client( String firstName, String lastName, String emailAddress, String accountPassword, String address, String creditCardInfo) {
+        super(firstName, lastName, emailAddress, accountPassword, address, userType.CLIENT);
+        this.creditCardInfo = creditCardInfo;
+    }
+
+    public String getCreditCardInfo() {
+        return creditCardInfo;
+    }
+
+    public void setCreditCardInfo(String newCCInfo) {
+        creditCardInfo = newCCInfo;
+    }
+
+    public String toString() {
+        return "\nAccount Information  \nFirst name: " + firstName + "\n" + "Last name: " + lastName + "\n" + "Email: " + emailAddress + "\n" + "Password: " + accountPassword + "\n" + "Credit Card Information " + creditCardInfo;
+    }
+}
