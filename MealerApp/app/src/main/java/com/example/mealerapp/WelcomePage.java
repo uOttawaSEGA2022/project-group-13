1490ca0ff71d55867a7a01673e1ebb44f4e4bea9
laurgu <lgu011@uottawa.ca>
2022-10-21 14:06:04 -0400

    Added logoff button function

--- MealerApp/app/src/main/java/com/example/mealerapp/WelcomePage.java
+++ MealerApp/app/src/main/java/com/example/mealerapp/WelcomePage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,12 +21,16 @@ public class WelcomePage extends AppCompatActivity {
         String role = bundle.getString("role");
 
 
-
-
-
         TextView userProfile = (TextView) findViewById(R.id.UserProfile);
-
         userProfile.setText(role);
+
+        Button logoff = (Button) findViewById(R.id.LogOff);
+        logoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogInPage(v);
+            }
+        });
     }
 
     public void LogInPage(View view){
