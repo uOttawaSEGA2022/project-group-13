b53c2014ed1ab7b72d7606bda27475d4e37405ef
JoannaReginald <113398424+JoannaReginald@users.noreply.github.com>
2022-10-31 19:12:43 -0400

    Lauren and Joanna

--- ComplaintsDataBase.java
+++ ComplaintsDataBase.java
@@ -1,86 +1,49 @@
 package com.example.secondderivable;
-package com.example.mealerapp;
-
-
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.googlefirebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Locale;
-//import com.google.firebase.storage.FirebaseStorage;
-//import com.google.firebase.storage.StorageReference;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class ComplaintDatabase extends Database implements Database.retrieveListener{
 
-    public class ComplaintsDataBase {
-        private DatabaseReference complaintsReference;
-        private FirebaseDatabase database;
-        private FirebaseAuth auth;
-        public enum dataField{COMPLAINTS, COOKNAME, ACTION};
-        private String uID = "null";
+    FirebaseDatabase database;
+    DatabaseReference complaintsRef;
 
-        public ComplaintsDataBase(){
-            database = FirebaseDatabase.getInstance();
-            complaintsReference = database.getReference().child("Complaints");
-            auth = FirebaseAuth.getInstance();
-        }
-
-        private void addComplaint(Complaints complaints, String cookName) {
-            ComplaintsReference.child(COOKNAME).setValueAsync(new Complaints(complaints, cookName, null)); // trying to add complaint to firebase, action hasnt been taken yet
-
-            databaseReference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    // inside the method of on Data change we are setting
-                    // our object class to our database reference.
-                    // data base reference will sends data to firebase.
-                    databaseReference.setValue(complaints);
-
-                    // after adding this data we are showing toast message.
-                    Toast.makeText( "data added", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    // if the data is not added or it is cancelled then
-                    // we are displaying a failure toast message.
-                    Toast.makeText( "Fail to add data " + error, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        //
-        public void changeInfo(User user,  dataField field, String newInfo){
-
-            //Make sure someone is logged in before trying to change their information
-            if(auth.getCurrentUser()!=null)
-                userReference.child(user.getRole().toString()).child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(field.toString().toLowerCase()).setValue(newInfo);
-        }
-
-        //Login user using email and password
-        public void login(String email, String password){
-
-            auth.signInWithEmailAndPassword(email,password);
-
-        }
-
-        //Log out current user
-        public void logoff(){
-
-            auth.signOut();
-
-        }
+    public ComplaintDatabase(){
+        complaintsRef = database.getReference("USERS").child("rQlNmFRGIBMhC0MhJnWdUVTgKM03").child("COMPLAINTS");
     }
 
+    private void getComplaints (final retrieveListener listener){
+        complaintsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Object> complaints = new ArrayList<Object>();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    complaints.add(dataSnapshot.getValue());
+                }
+                listener.onDataReceived(complaints);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+    public void addComplaint(String cookUID, Complaint complaint){
+        setInformation(complaintsRef.child(cookUID), complaint);
+    }
+    public void deleteComplaint(String cookUID){
+        deleteInformation(complaintsRef.child(cookUID));
+    }
+    @Override
+    public void onDataReceived(Object data) {
+    }
+    @Override
+    public void onError() {
+    }
+}
