9393251578fae0dee1422c1baf5b45a052bda475
Algo Woolf <114178509+AlgoWoolf@users.noreply.github.com>
2022-11-06 23:18:51 -0500

    Update UserDatabase.java

--- MealerApp/app/src/main/java/com/example/mealerapp/UserDatabase.java
+++ MealerApp/app/src/main/java/com/example/mealerapp/UserDatabase.java
@@ -61,10 +61,21 @@ public class UserDatabase extends Database{
         getInformation(fieldReference, listener);
 
     }
+    
     //Login user using email and password
     public void login(String email, String password){
 
-            auth.signInWithEmailAndPassword(email,password);
+            auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Log.d("Login Status: ", "Success");
+                    }
+                    else{
+                        Log.d("Login Status: ", "Fail");
+                    }
+                }
+            });
     }
 
     //Log out current user
